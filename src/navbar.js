0e07ae1cc2a11f9b335122749eb6b0b906e35c0d
Jean-Denis Vauguet <jd@vauguet.fr>
2015-08-28 14:15:23 +0000

    Refactor navbar with React

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+React.render(
+    <nav className="navbar">
+        <a href="index.html" className="logo">
+    	    <img src="assets/images/bloc_jams_logo.png" alt="bloc jams logo"/>
+        </a>
+        
+        <div className="links-container">
+        	<a href="collection.html" className="navbar-link">collection</a>
+        </div>
+    </nav>,
+    document.getElementsByClassName('collection')[0]
+);
+
